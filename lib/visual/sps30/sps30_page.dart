a00b7effb1f4740ac70546b195f636b6c9cdde9e
Vincent Långström <vincent.langstrom@gmail.com>
2021-05-28 15:26:08 +0300

    change sps30 page structure

--- lib/visual/sps30/sps30_page.dart
+++ lib/visual/sps30/sps30_page.dart
@@ -1,19 +1,34 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_iot_ui/data/sps30/sps30_datamodel.dart';
 import 'package:flutter_iot_ui/visual/appbar_trailing.dart';
 import 'package:flutter_iot_ui/visual/drawer.dart';
 import 'package:flutter_iot_ui/data/sqlite.dart';
 import 'package:flutter_iot_ui/data/constants.dart' show dbPath;
-import 'package:flutter_iot_ui/visual/sps30/sps30_data_chart.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 
 class SPS30Page extends StatefulWidget {
-  final String title = 'SPS30 Sensor Data';
+  final String title = 'SCD30 Sensor Data';
 
   @override
   _SPS30PageState createState() => _SPS30PageState();
 }
 
 class _SPS30PageState extends State<SPS30Page> {
+  bool _showMassConcentrationPM1_0 = true;
+  bool _showMassConcentrationPM2_5 = true;
+  bool _showMassConcentrationPM4_0 = true;
+  bool _showMassConcentrationPM10 = true;
+  bool _showNumberConcentrationPM0_5 = true;
+  bool _showNumberConcentrationPM1_0 = true;
+  bool _showNumberConcentrationPM2_5 = true;
+  bool _showNumberConcentrationPM4_0 = true;
+  bool _showNumberConcentrationPM10 = true;
+  bool _showtypicalParticleSize = true;
+
+  // TODO: move to separate data provider (maybe usin provider or bloc)
+  List<SPS30SensorDataEntry> _dataList = [];
+
   bool _continue = true;
 
   // TODO: don't fetch the whole database every time...
@@ -27,6 +42,16 @@ class _SPS30PageState extends State<SPS30Page> {
           Duration(seconds: 5), () => getAllSPS30Entries(dbPath));
       yield db;
     }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    dbUpdates().listen((data) {
+      setState(() {
+        _dataList = data;
+      });
+    });
   }
 
   @override
@@ -54,18 +79,116 @@ class _SPS30PageState extends State<SPS30Page> {
       ),
       drawer: NavDrawer(),
       body: Center(
-        child: StreamBuilder(
-          stream: dbUpdates(),
-          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-            if (snapshot.hasData) {
-              return SPS30DataChart(snapshot.data);
-            } else if (snapshot.hasError) {
-              return Text('Error');
-            } else {
-              return CircularProgressIndicator();
-            }
-          },
-        ),
+        child: this._dataList.isNotEmpty
+            ? Column(
+                children: [
+                  Flexible(
+                    child: charts.TimeSeriesChart(
+                      [
+                        if (_showMassConcentrationPM1_0)
+                          charts.Series<SPS30SensorDataEntry, DateTime>(
+                              id: 'Mass Concentration PM1.0 (µg/m³)',
+                              colorFn: (_, __) =>
+                                  charts.MaterialPalette.blue.shadeDefault,
+                              domainFn: (SPS30SensorDataEntry value, _) =>
+                                  value.timeStamp,
+                              measureFn: (SPS30SensorDataEntry value, _) =>
+                                  value.massConcentrationPM1_0,
+                              data: _dataList),
+                        if (_showMassConcentrationPM2_5)
+                          charts.Series<SPS30SensorDataEntry, DateTime>(
+                              id: 'Mass Concentration PM2.5 (µg/m³)',
+                              colorFn: (_, __) =>
+                                  charts.MaterialPalette.green.shadeDefault,
+                              domainFn: (SPS30SensorDataEntry value, _) =>
+                                  value.timeStamp,
+                              measureFn: (SPS30SensorDataEntry value, _) =>
+                                  value.massConcentrationPM2_5,
+                              data: _dataList),
+                        if (_showMassConcentrationPM4_0)
+                          charts.Series<SPS30SensorDataEntry, DateTime>(
+                              id: 'Mass Concentration PM4.0 (µg/m³)',
+                              colorFn: (_, __) =>
+                                  charts.MaterialPalette.purple.shadeDefault,
+                              domainFn: (SPS30SensorDataEntry value, _) =>
+                                  value.timeStamp,
+                              measureFn: (SPS30SensorDataEntry value, _) =>
+                                  value.massConcentrationPM4_0,
+                              data: _dataList),
+                        if (_showMassConcentrationPM10)
+                          charts.Series<SPS30SensorDataEntry, DateTime>(
+                              id: 'Mass Concentration PM10 (µg/m³)',
+                              colorFn: (_, __) => charts
+                                  .MaterialPalette.deepOrange.shadeDefault,
+                              domainFn: (SPS30SensorDataEntry value, _) =>
+                                  value.timeStamp,
+                              measureFn: (SPS30SensorDataEntry value, _) =>
+                                  value.massConcentrationPM10,
+                              data: _dataList),
+                      ],
+                      animate: true,
+                      // Optionally pass in a [DateTimeFactory] used by the chart. The factory
+                      // should create the same type of [DateTime] as the data provided. If none
+                      // specified, the default creates local date time.
+                      dateTimeFactory: const charts.LocalDateTimeFactory(),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Row(children: [
+                        Checkbox(
+                          activeColor: Colors.blue,
+                          value: _showMassConcentrationPM1_0,
+                          onChanged: (bool value) {
+                            setState(() {
+                              _showMassConcentrationPM1_0 = value;
+                            });
+                          },
+                        ),
+                        Text('Mass Concentration PM1.0 (µg/m³)'),
+                      ]),
+                      Row(children: [
+                        Checkbox(
+                          activeColor: Colors.green,
+                          value: _showMassConcentrationPM2_5,
+                          onChanged: (bool value) {
+                            setState(() {
+                              _showMassConcentrationPM2_5 = value;
+                            });
+                          },
+                        ),
+                        Text('Mass Concentration PM2.5 (µg/m³)'),
+                      ]),
+                      Row(children: [
+                        Checkbox(
+                          activeColor: Colors.purple,
+                          value: _showMassConcentrationPM4_0,
+                          onChanged: (bool value) {
+                            setState(() {
+                              _showMassConcentrationPM4_0 = value;
+                            });
+                          },
+                        ),
+                        Text('Mass Concentration PM4.0 (µg/m³)'),
+                      ]),
+                      Row(children: [
+                        Checkbox(
+                          activeColor: Colors.deepOrange,
+                          value: _showMassConcentrationPM10,
+                          onChanged: (bool value) {
+                            setState(() {
+                              _showMassConcentrationPM10 = value;
+                            });
+                          },
+                        ),
+                        Text('Mass Concentration PM10 (µg/m³)'),
+                      ]),
+                    ],
+                  )
+                ],
+              )
+            : CircularProgressIndicator(),
       ),
     );
   }
