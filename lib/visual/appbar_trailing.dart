897afce072150de4d21f870a3a348c40cd9e95af
Vincent Långström <vincent.langstrom@gmail.com>
2021-06-01 12:02:57 +0300

    better clock formatting

--- lib/visual/appbar_trailing.dart
+++ lib/visual/appbar_trailing.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class AppbarTrailingInfo extends StatefulWidget {
   @override
@@ -10,42 +11,23 @@ class AppbarTrailingInfo extends StatefulWidget {
 }
 
 class AppbarTrailingInfoState extends State<AppbarTrailingInfo> {
-  DateTime _time;
-  Timer _timer;
-
-  void _getTime() {
-    var now = DateTime.now();
-    setState(() {
-      _time = now;
-    });
-  }
-
-  String _formatDateTime(DateTime dateTime) {
-    return '${dateTime.day}.${dateTime.month}.${dateTime.year} ${dateTime.hour}:${dateTime.minute}:${dateTime.second}';
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _time = DateTime.now();
-    _timer = Timer.periodic(Duration(seconds: 1), (Timer t) => _getTime());
-  }
-
-  @override
-  void dispose() {
-    _timer.cancel();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 40),
-          child: Text(
-            _formatDateTime(_time),
-            textScaleFactor: 2,
+          child: StreamBuilder(
+            // This stream is listened to and canceled automatically
+            stream: Stream.periodic(const Duration(seconds: 1)),
+            builder: (context, snapshot) {
+              return Center(
+                child: Text(
+                  DateFormat('MM/dd/yyyy hh:mm:ss').format(DateTime.now()),
+                  textScaleFactor: 2,
+                ),
+              );
+            },
           ),
         ),
       ],
