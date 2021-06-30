3acb6abdafda66085737f7de1cb7319aff323126
Vincent Långström <vincent.langstrom@gmail.com>
2021-06-30 15:12:16 +0300

    start refactor to general pages

---
+++ lib/visual/general_graph_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_iot_ui/visual/appbar_trailing.dart';
+import 'package:flutter_iot_ui/visual/drawer.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+
+class GeneralGraphPage extends StatefulWidget {
+  final String title = 'SCD30 Sensor Data';
+  final Stream<List<charts.Series<dynamic, DateTime>>> seriesListStream;
+
+  GeneralGraphPage({Key key, @required this.seriesListStream})
+      : super(key: key);
+
+  @override
+  _GeneralGraphPageState createState() => _GeneralGraphPageState();
+}
+
+class _GeneralGraphPageState extends State<GeneralGraphPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: [AppbarTrailingInfo()],
+      ),
+      drawer: NavDrawer(),
+      body: StreamBuilder(
+        stream: widget.seriesListStream,
+        builder: (context, snapshot) {
+          if (snapshot.hasData && (snapshot.data as List).isNotEmpty) {
+            return charts.TimeSeriesChart(
+              snapshot.data,
+              animate: true,
+              behaviors: [
+                charts.SeriesLegend(
+                  position: charts.BehaviorPosition.bottom,
+                  horizontalFirst: true,
+                  desiredMaxColumns: 2,
+                  showMeasures: true,
+                  // TODO: change to last
+                  // Using last doesn't work when we hide one of the lines
+                  legendDefaultMeasure: charts.LegendDefaultMeasure.none,
+                  measureFormatter: (num value) {
+                    // Despite some initial confusion, it turns out that this actually rounds the numbers
+                    return value == null || value.isNaN
+                        ? '-'
+                        : value.toStringAsFixed(1);
+                  },
+                ),
+              ],
+            );
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error.'));
+          } else {
+            return Center(
+              child: Column(mainAxisSize: MainAxisSize.min, children: [
+                CircularProgressIndicator(),
+                Text('No data to show (yet).')
+              ]),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
--- lib/visual/scd30_page.dart
+++ lib/visual/scd30_page.dart
@@ -1,11 +1,10 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_iot_ui/data/scd30_datamodel.dart';
-import 'package:flutter_iot_ui/visual/appbar_trailing.dart';
-import 'package:flutter_iot_ui/visual/drawer.dart';
 import 'package:flutter_iot_ui/data/sqlite.dart';
 import 'package:flutter_iot_ui/data/constants.dart' show dbPath;
 import 'package:charts_flutter/flutter.dart' as charts;
+import 'package:flutter_iot_ui/visual/general_graph_page.dart';
 
 class SCD30Page extends StatefulWidget {
   final String title = 'SCD30 Sensor Data';
@@ -33,106 +32,25 @@ class _SCD30PageState extends State<SCD30Page> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-        actions: [AppbarTrailingInfo()],
-      ),
-      drawer: NavDrawer(),
-      body: StreamBuilder(
-        stream: dbUpdates(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData && (snapshot.data as List).isNotEmpty) {
-            return charts.TimeSeriesChart(
-              [
-                charts.Series<SCD30SensorDataEntry, DateTime>(
-                    id: 'Carbon Dioxide (ppm)',
-                    colorFn: (_, __) =>
-                        charts.MaterialPalette.blue.shadeDefault,
-                    domainFn: (SCD30SensorDataEntry value, _) =>
-                        value.timeStamp,
-                    measureFn: (SCD30SensorDataEntry value, _) =>
-                        value.carbonDioxide,
-                    data: snapshot.data),
-                charts.Series<SCD30SensorDataEntry, DateTime>(
-                    id: 'Temperature (°C)',
-                    colorFn: (_, __) => charts.MaterialPalette.red.shadeDefault,
-                    domainFn: (SCD30SensorDataEntry value, _) =>
-                        value.timeStamp,
-                    measureFn: (SCD30SensorDataEntry value, _) =>
-                        value.temperature,
-                    data: snapshot.data),
-                charts.Series<SCD30SensorDataEntry, DateTime>(
-                    id: 'Humidity (%RH)',
-                    colorFn: (_, __) =>
-                        charts.MaterialPalette.yellow.shadeDefault,
-                    domainFn: (SCD30SensorDataEntry value, _) =>
-                        value.timeStamp,
-                    measureFn: (SCD30SensorDataEntry value, _) =>
-                        value.humidity,
-                    data: snapshot.data),
-              ],
-              animate: true,
-              // Optionally pass in a [DateTimeFactory] used by the chart. The factory
-              // should create the same type of [DateTime] as the data provided. If none
-              // specified, the default creates local date time.
-              dateTimeFactory: const charts.LocalDateTimeFactory(),
-              behaviors: [
-                charts.SeriesLegend(
-                  // Positions for "start" and "end" will be left and right respectively
-                  // for widgets with a build context that has directionality ltr.
-                  // For rtl, "start" and "end" will be right and left respectively.
-                  // Since this example has directionality of ltr, the legend is
-                  // positioned on the right side of the chart.
-                  position: charts.BehaviorPosition.bottom,
-                  // By default, if the position of the chart is on the left or right of
-                  // the chart, [horizontalFirst] is set to false. This means that the
-                  // legend entries will grow as new rows first instead of a new column.
-                  horizontalFirst: true,
-                  showMeasures: true,
-                  // TODO: change to last
-                  // Using last doesn't work when we hide one of the lines
-                  legendDefaultMeasure: charts.LegendDefaultMeasure.none,
-                  measureFormatter: (num value) {
-                    // Despite some initial confusion, it turns out that this actually rounds the numbers
-                    return value == null || value.isNaN
-                        ? '-'
-                        : value.toStringAsFixed(1);
-                  },
-                ),
-                // charts.InitialSelection(
-                //   selectedDataConfig: [
-                //     charts.SeriesDatumConfig<DateTime>(
-                //         'Carbon Dioxide (ppm)',
-                //         _dataList?.last?.timeStamp),
-                //     charts.SeriesDatumConfig<DateTime>(
-                //         'Temperature (°C)', _dataList?.last?.timeStamp),
-                //     charts.SeriesDatumConfig<DateTime>(
-                //         'Humidity (%RH)', _dataList?.last?.timeStamp)
-                //   ],
-                // ),
-              ],
-            );
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error.'));
-          } else {
-            return Center(
-              child: Column(mainAxisSize: MainAxisSize.min, children: [
-                CircularProgressIndicator(),
-                Text('No data (yet).')
-              ]),
-            );
-          }
-        },
-      ),
-    );
+    return GeneralGraphPage(
+        seriesListStream: dbUpdates().map((event) => [
+              charts.Series<SCD30SensorDataEntry, DateTime>(
+                  id: 'Carbon Dioxide (ppm)',
+                  domainFn: (SCD30SensorDataEntry value, _) => value.timeStamp,
+                  measureFn: (SCD30SensorDataEntry value, _) =>
+                      value.carbonDioxide,
+                  data: event),
+              charts.Series<SCD30SensorDataEntry, DateTime>(
+                  id: 'Temperature (°C)',
+                  domainFn: (SCD30SensorDataEntry value, _) => value.timeStamp,
+                  measureFn: (SCD30SensorDataEntry value, _) =>
+                      value.temperature,
+                  data: event),
+              charts.Series<SCD30SensorDataEntry, DateTime>(
+                  id: 'Humidity (%RH)',
+                  domainFn: (SCD30SensorDataEntry value, _) => value.timeStamp,
+                  measureFn: (SCD30SensorDataEntry value, _) => value.humidity,
+                  data: event),
+            ]));
   }
 }
