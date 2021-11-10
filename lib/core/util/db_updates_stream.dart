3499d20c85797f5caf11d7330e7c7fe56c0b5755
Vincent Långström <vincent.langstrom@gmail.com>
2021-11-10 13:18:22 +0200

    simpler db update stream, await delay after yield
    This ensure that, after the stream is canceled, we don't wait the entire graphRefreshDuration before we reach the next yield statement and the stream is done

--- lib/core/util/db_updates_stream.dart
+++ lib/core/util/db_updates_stream.dart
@@ -7,20 +7,18 @@ Stream<List<T>> dbUpdatesOfType<T extends GenericSensorDataEntry>(
     required Duration graphTimeWindow}) async* {
   var dbManager = GetIt.instance<DatabaseManager>();
 
-  // Init
-  var today = DateTime.now();
-  var yesterday = today.subtract(graphTimeWindow);
-  // Just creating an instance of this singleton class will initialize it and the database.
-  var db = await dbManager.getEntriesOfType<T>(start: yesterday, stop: today);
-  yield db;
+  DateTime today;
+  DateTime yesterday;
 
   // This stream will be automatically cancelled by dart when no longer needed.
   // Furhtermore this loop will automatically stop running when the stream is canceled.
   while (true) {
     today = DateTime.now();
     yesterday = today.subtract(graphTimeWindow);
-    db = await Future.delayed(refreshDuration,
-        () => dbManager.getEntriesOfType<T>(start: yesterday, stop: today));
-    yield db;
+
+    print('dbUpdates loop $today');
+
+    yield await dbManager.getEntriesOfType<T>(start: yesterday, stop: today);
+    await Future.delayed(refreshDuration);
   }
 }
