09b12901c1167f3b5ba89ec38c9646ab105c56b6
Vincent Långström <vincent.langstrom@gmail.com>
2021-11-25 17:23:16 +0200

    update contract ABI and generated files

--- lib/core/models/contracts/Oracle.g.dart
+++ lib/core/models/contracts/Oracle.g.dart
@@ -1,11 +1,9 @@
 // Generated code, do not modify. Run `build_runner build` to re-generate!
 // @dart=2.12
-// ignore_for_file: non_constant_identifier_names, camel_case_types
-
 import 'package:web3dart/web3dart.dart' as _i1;
 
 final _contractAbi = _i1.ContractAbi.fromJson(
-    '[{"inputs":[{"internalType":"uint256","name":"_price","type":"uint256"},{"internalType":"address","name":"_owner","type":"address"},{"internalType":"address","name":"_task_manager","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[],"name":"middleware","type":"event"},{"anonymous":false,"inputs":[],"name":"modification","type":"event"},{"inputs":[],"name":"active","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"backlog","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"completed","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"config","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"discoverable","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"price","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"task_manager","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"fetch_backlog","outputs":[{"internalType":"address[]","name":"","type":"address[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"update_middleware","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"data","type":"string"}],"name":"update_config","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"toggle_active","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"toggle_discoverable","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"task","type":"address"}],"name":"assign_task","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"target","type":"address"},{"internalType":"uint256","name":"reward","type":"uint256"}],"name":"clear_task","outputs":[],"stateMutability":"nonpayable","type":"function"}]',
+    '[{"inputs":[{"internalType":"uint256","name":"_price","type":"uint256"},{"internalType":"address","name":"_owner","type":"address"},{"internalType":"address","name":"_task_manager","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[],"name":"middleware","type":"event"},{"anonymous":false,"inputs":[],"name":"modification","type":"event"},{"inputs":[],"name":"active","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"backlog","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"completed","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"price","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"task_manager","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"fetch_backlog","outputs":[{"internalType":"address[]","name":"","type":"address[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"update_middleware","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"toggle_active","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"task","type":"address"}],"name":"assign_task","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"target","type":"address"},{"internalType":"uint256","name":"reward","type":"uint256"}],"name":"clear_task","outputs":[],"stateMutability":"nonpayable","type":"function"}]',
     'Oracle');
 
 class Oracle extends _i1.GeneratedContract {
@@ -52,30 +50,8 @@ class Oracle extends _i1.GeneratedContract {
   /// The optional [atBlock] parameter can be used to view historical data. When
   /// set, the function will be evaluated in the specified block. By default, the
   /// latest on-chain block will be used.
-  Future<String> config({_i1.BlockNum? atBlock}) async {
-    final function = self.abi.functions[4];
-    assert(checkSignature(function, '79502c55'));
-    final params = [];
-    final response = await read(function, params, atBlock);
-    return (response[0] as String);
-  }
-
-  /// The optional [atBlock] parameter can be used to view historical data. When
-  /// set, the function will be evaluated in the specified block. By default, the
-  /// latest on-chain block will be used.
-  Future<bool> discoverable({_i1.BlockNum? atBlock}) async {
-    final function = self.abi.functions[5];
-    assert(checkSignature(function, '7882ece1'));
-    final params = [];
-    final response = await read(function, params, atBlock);
-    return (response[0] as bool);
-  }
-
-  /// The optional [atBlock] parameter can be used to view historical data. When
-  /// set, the function will be evaluated in the specified block. By default, the
-  /// latest on-chain block will be used.
   Future<_i1.EthereumAddress> owner({_i1.BlockNum? atBlock}) async {
-    final function = self.abi.functions[6];
+    final function = self.abi.functions[4];
     assert(checkSignature(function, '8da5cb5b'));
     final params = [];
     final response = await read(function, params, atBlock);
@@ -86,7 +62,7 @@ class Oracle extends _i1.GeneratedContract {
   /// set, the function will be evaluated in the specified block. By default, the
   /// latest on-chain block will be used.
   Future<BigInt> price({_i1.BlockNum? atBlock}) async {
-    final function = self.abi.functions[7];
+    final function = self.abi.functions[5];
     assert(checkSignature(function, 'a035b1fe'));
     final params = [];
     final response = await read(function, params, atBlock);
@@ -97,7 +73,7 @@ class Oracle extends _i1.GeneratedContract {
   /// set, the function will be evaluated in the specified block. By default, the
   /// latest on-chain block will be used.
   Future<_i1.EthereumAddress> task_manager({_i1.BlockNum? atBlock}) async {
-    final function = self.abi.functions[8];
+    final function = self.abi.functions[6];
     assert(checkSignature(function, 'b17f7d31'));
     final params = [];
     final response = await read(function, params, atBlock);
@@ -109,7 +85,7 @@ class Oracle extends _i1.GeneratedContract {
   /// latest on-chain block will be used.
   Future<List<_i1.EthereumAddress>> fetch_backlog(
       {_i1.BlockNum? atBlock}) async {
-    final function = self.abi.functions[9];
+    final function = self.abi.functions[7];
     assert(checkSignature(function, 'e7869fad'));
     final params = [];
     final response = await read(function, params, atBlock);
@@ -122,21 +98,9 @@ class Oracle extends _i1.GeneratedContract {
   Future<String> update_middleware(
       {required _i1.Credentials credentials,
       _i1.Transaction? transaction}) async {
-    final function = self.abi.functions[10];
+    final function = self.abi.functions[8];
     assert(checkSignature(function, '50454d89'));
     final params = [];
-    return write(credentials, transaction, function, params);
-  }
-
-  /// The optional [transaction] parameter can be used to override parameters
-  /// like the gas price, nonce and max gas. The `data` and `to` fields will be
-  /// set by the contract.
-  Future<String> update_config(String data,
-      {required _i1.Credentials credentials,
-      _i1.Transaction? transaction}) async {
-    final function = self.abi.functions[11];
-    assert(checkSignature(function, 'e639e4a6'));
-    final params = [data];
     return write(credentials, transaction, function, params);
   }
 
@@ -146,20 +110,8 @@ class Oracle extends _i1.GeneratedContract {
   Future<String> toggle_active(
       {required _i1.Credentials credentials,
       _i1.Transaction? transaction}) async {
-    final function = self.abi.functions[12];
+    final function = self.abi.functions[9];
     assert(checkSignature(function, '3afd1b67'));
-    final params = [];
-    return write(credentials, transaction, function, params);
-  }
-
-  /// The optional [transaction] parameter can be used to override parameters
-  /// like the gas price, nonce and max gas. The `data` and `to` fields will be
-  /// set by the contract.
-  Future<String> toggle_discoverable(
-      {required _i1.Credentials credentials,
-      _i1.Transaction? transaction}) async {
-    final function = self.abi.functions[13];
-    assert(checkSignature(function, '9f2c52df'));
     final params = [];
     return write(credentials, transaction, function, params);
   }
@@ -170,7 +122,7 @@ class Oracle extends _i1.GeneratedContract {
   Future<String> assign_task(_i1.EthereumAddress task,
       {required _i1.Credentials credentials,
       _i1.Transaction? transaction}) async {
-    final function = self.abi.functions[14];
+    final function = self.abi.functions[10];
     assert(checkSignature(function, '176e8285'));
     final params = [task];
     return write(credentials, transaction, function, params);
@@ -182,7 +134,7 @@ class Oracle extends _i1.GeneratedContract {
   Future<String> clear_task(_i1.EthereumAddress target, BigInt reward,
       {required _i1.Credentials credentials,
       _i1.Transaction? transaction}) async {
-    final function = self.abi.functions[15];
+    final function = self.abi.functions[11];
     assert(checkSignature(function, 'd52cf8ba'));
     final params = [target, reward];
     return write(credentials, transaction, function, params);
